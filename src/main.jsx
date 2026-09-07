967862a7ae2d0d58eef99d5f96a33a672d578952
ensaf01 <mohammedensaf402@gmail.com>
2026-09-07 21:53:19 +0600

    add <Outlet> and Add footer from daisyui

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,19 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Root/Home/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children :[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
   },
 ]);
 
